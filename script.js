efffd56c9207f5e06418883673605a28e1cffd4e
Mcoronel0 <163454580+Mcoronel0@users.noreply.github.com>
2024-11-29 17:45:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-    e.preventDefault();  // Evita que el formulario recargue la página
-
-    const formData = new FormData(this);
-
-    // Usamos fetch para enviar el formulario de manera asíncrona
-    fetch(this.action, {
-        method: this.method,
-        body: formData
-    })
-    .then(response => {
-        if (response.ok) {
-            // Mostrar el snackbar de agradecimiento
-            const snackbar = document.getElementById("snackbar");
-            snackbar.className = "show"; // Activar la clase 'show' para mostrar el snackbar
-
-            // Reiniciar el formulario después de mostrar el snackbar
-            this.reset();
-
-            // Ocultar el snackbar después de 3 segundos
-            setTimeout(function() {
-                snackbar.className = snackbar.className.replace("show", ""); 
-            }, 3000);
-        } else {
-            alert("Hubo un problema al enviar tu mensaje. Por favor, intenta nuevamente.");
-        }
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        alert("Hubo un error. Por favor, intenta más tarde.");
-    });
-});
+        document.getElementById("contact-form").addEventListener("submit", function (e) {
+            e.preventDefault();
+            const formData = new FormData(this);
+            fetch("https://formsubmit.co/mauro.nicolas.coronel@gmail.com", {
+                method: "POST",
+                body: formData
+            })
+                .then(response => {
+                    if (response.ok) {
+                        const snackbar = document.getElementById("snackbar");
+                        snackbar.className = "show";
+                        this.reset();
+                        setTimeout(() => snackbar.className = snackbar.className.replace("show", ""), 3000);
+                    } else {
+                        alert("Error al enviar el formulario.");
+                    }
+                })
+                .catch(() => alert("Hubo un problema. Intente nuevamente."));
+        });
